0575701bf95ae721a406b309bc70d5d3a4bd0ff8
Martin Purita <mpurita@monits.com>
2015-07-07 09:35:45 -0300

    Created a gradle plugin to include the linters config
    Summary: Created a gradle plugin to include the linters config
    
    Test Plan: -
    
    Reviewers: lbritez, jmsotuyo
    
    Reviewed By: jmsotuyo
    
    Subscribers: jmsotuyo
    
    Differential Revision: http://ph.monits.com/D10804

--- src/main/groovy/com/monits/StaticCodeAnalysis.groovy
+++ src/main/groovy/com/monits/StaticCodeAnalysis.groovy
@@ -1,13 +1,14 @@
-package com.monits;
+package com.monits
 
-import org.gradle.api.Project
 import org.gradle.api.Plugin
+import org.gradle.api.Project
 import org.gradle.api.file.FileTree
 import org.gradle.api.plugins.quality.Checkstyle
 import org.gradle.api.plugins.quality.FindBugs
 import org.gradle.api.plugins.quality.Pmd
-import org.gradle.api.tasks.compile.JavaCompile
 import org.gradle.api.tasks.bundling.Jar
+import org.gradle.api.tasks.compile.JavaCompile
+import org.gradle.api.tasks.wrapper.Wrapper
 
 class StaticCodeAnalysis implements Plugin<Project> {
 
@@ -32,16 +33,16 @@ class StaticCodeAnalysis implements Plugin<Project> {
         project.dependencies {
             provided 'com.google.code.findbugs:annotations:3.0.0'
 
-            checkstyle 'com.puppycrawl.tools:checkstyle:6.2'
+            checkstyle 'com.puppycrawl.tools:checkstyle:6.7'
 
-            findbugs 'com.google.code.findbugs:findbugs:3.0.0'
+            findbugs 'com.google.code.findbugs:findbugs:3.0.1'
             findbugs project.configurations.findbugsPlugins.dependencies
 
             // To keep everything tidy, we set these apart
-            findbugsPlugins('com.monits:findbugs-plugin:0.1.0-SNAPSHOT') {
+            findbugsPlugins('com.monits:findbugs-plugin:0.2.0-SNAPSHOT') {
                 transitive = false
             }
-            findbugsPlugins 'com.mebigfatguy.fb-contrib:fb-contrib:6.0.0'
+            findbugsPlugins 'com.mebigfatguy.fb-contrib:fb-contrib:6.2.1'
         }
 
         project.task("sourcesJar", type: Jar) {
@@ -99,7 +100,31 @@ class StaticCodeAnalysis implements Plugin<Project> {
             }
 
             pluginClasspath = project.configurations.findbugsPlugins
-            classpath = project.configurations.compile
+        }
+
+        /*
+         * For best results, Findbugs needs ALL classes, including Android's SDK,
+         * but the task is created dynamically, so we need to set it afterEvaluate
+         */
+        project.afterEvaluate {
+            project.tasks.withType(FindBugs).each {
+                def t = project.tasks.findByName('mockableAndroidJar');
+                if (t != null) {
+                    it.dependsOn project.tasks.findByName('mockableAndroidJar')
+                }
+                it.classpath = project.configurations.compile + project.configurations.testCompile +
+                        project.fileTree(dir: "${project.buildDir}/intermediates/exploded-aar/", include: '**/*.jar') +
+                        project.fileTree(dir: "${project.buildDir}/intermediates/", include: 'mockable-android-*.jar')
+            }
+        }
+
+        // PMD 5.2+ requires gradle 2.4+
+        project.task("wrapper", type: Wrapper) {
+            gradleVersion = '2.4'
+        }
+
+        project.pmd {
+             toolVersion = '5.3.2'
         }
 
         project.task("pmd", type: Pmd) {
