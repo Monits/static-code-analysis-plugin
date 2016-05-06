567e224aca5693d962b74392b02f8bcac0fcf5fe
Juan Martín Sotuyo Dodero <jmsotuyo@monits.com>
2016-05-06 17:22:29 -0300

    Add test for DSL under checkstyle

--- src/integTest/groovy/com/monits/gradle/sca/CheckstyleIntegTest.groovy
+++ src/integTest/groovy/com/monits/gradle/sca/CheckstyleIntegTest.groovy
@@ -14,6 +14,7 @@
 package com.monits.gradle.sca
 
 import com.monits.gradle.sca.fixture.AbstractPerSourceSetPluginIntegTestFixture
+import com.monits.gradle.sca.io.TestFile
 import org.gradle.testkit.runner.BuildResult
 import org.gradle.util.GradleVersion
 import spock.lang.Unroll
@@ -21,6 +22,7 @@ import spock.lang.Unroll
 import static org.gradle.testkit.runner.TaskOutcome.FAILED
 import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
 import static org.hamcrest.CoreMatchers.containsString
+import static org.hamcrest.core.IsNot.not
 import static org.junit.Assert.assertThat
 
 /**
@@ -32,7 +34,7 @@ class CheckstyleIntegTest extends AbstractPerSourceSetPluginIntegTestFixture {
     void 'checkstyle is run'() {
         given:
         writeBuildFile()
-        useEmptyCheckstyleConfig()
+        useAlmostEmptyCheckstyleConfig()
         goodCode()
 
         when:
@@ -100,8 +102,8 @@ class CheckstyleIntegTest extends AbstractPerSourceSetPluginIntegTestFixture {
     void 'running offline with a cached file passes but warns'() {
         given:
         writeBuildFile()
-        writeEmptyCheckstyleConfig('main')
-        writeEmptyCheckstyleConfig('test')
+        writeAlmostEmptyCheckstyleConfig('main')
+        writeAlmostEmptyCheckstyleConfig('test')
         goodCode()
 
         when:
@@ -113,6 +115,40 @@ class CheckstyleIntegTest extends AbstractPerSourceSetPluginIntegTestFixture {
         result.task(':downloadCheckstyleXmlTest').outcome == SUCCESS
         result.task(':downloadCheckstyleXmlMain').outcome == SUCCESS
         assertThat(result.output, containsString('Running in offline mode. Using a possibly outdated version of'))
+    }
+
+    @SuppressWarnings('MethodName')
+    void 'dsl allows to override rules per sourceset'() {
+        given:
+        writeBuildFile() << '''
+            staticCodeAnalysis {
+                sourceSetConfig {
+                    test {
+                        checkstyleRules = 'test-checkstyle.xml'
+                    }
+                }
+            }
+        '''
+        writeAlmostEmptyCheckstyleConfig('test').renameTo(file('test-checkstyle.xml'))
+        goodCode()
+
+        when:
+        BuildResult result = gradleRunner()
+                .build()
+
+        then:
+        result.task(taskName()).outcome == SUCCESS
+
+        // The config for other sourcesets must exist
+        file('config/checkstyle/checkstyle-main.xml').exists()
+
+        // Make sure checkstyle reports exist
+        reportFile().exists()
+        reportFile('test').exists()
+
+        // But results hsould differ in spite of being very similar code
+        reportFile().assertContents(containsString('<error '))
+        reportFile('test').assertContents(not(containsString('<error ')))
     }
 
     @Override
@@ -130,7 +166,7 @@ class CheckstyleIntegTest extends AbstractPerSourceSetPluginIntegTestFixture {
         'checkstyle'
     }
 
-    void writeEmptyCheckstyleConfig(final String sourceSet = null) {
+    TestFile writeAlmostEmptyCheckstyleConfig(final String sourceSet = null) {
         file("config/checkstyle/checkstyle${sourceSet ? "-${sourceSet}" : ''}.xml") <<
                 '''<?xml version="1.0" encoding="UTF-8"?>
             <!DOCTYPE module PUBLIC "-//Puppy Crawl//DTD Check Configuration 1.3//EN"
@@ -141,11 +177,11 @@ class CheckstyleIntegTest extends AbstractPerSourceSetPluginIntegTestFixture {
                     <module name="OneTopLevelClass"/>
                 </module>
             </module>
-        '''
+        ''' as TestFile
     }
 
-    void useEmptyCheckstyleConfig() {
-        writeEmptyCheckstyleConfig()
+    void useAlmostEmptyCheckstyleConfig() {
+        writeAlmostEmptyCheckstyleConfig()
 
         buildScriptFile() << '''
             staticCodeAnalysis {
