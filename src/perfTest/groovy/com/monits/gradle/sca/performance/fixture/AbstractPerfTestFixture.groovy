0d87ea12ec8aff9e5b2b5b5db0dfb626a5c90c6b
Juan Martín Sotuyo Dodero <jmsotuyo@monits.com>
2016-11-24 01:17:23 -0300

    Use latest gradle in Android perf test too
     - Also make sure to test against latest release

--- src/perfTest/groovy/com/monits/gradle/sca/performance/fixture/AbstractPerfTestFixture.groovy
+++ src/perfTest/groovy/com/monits/gradle/sca/performance/fixture/AbstractPerfTestFixture.groovy
@@ -32,8 +32,9 @@ abstract class AbstractPerfTestFixture extends Specification {
     static final List<String> TESTED_GRADLE_VERSIONS = ['2.8', '2.14.1', GradleVersion.current().version].asImmutable()
     @SuppressWarnings(['DuplicateStringLiteral', 'UnnecessaryCast'])
     static final List<String> TESTED_GRADLE_VERSIONS_FOR_ANDROID = (['2.8', '2.14.1'] +
-        (Jvm.current.java8Compatible ? ['3.1'] : [] as List<String>)).takeRight(2).asImmutable()
-    static final String BASELINE_PLUGIN_VERSION = '"com.monits:static-code-analysis-plugin:2.3.2"'
+        (Jvm.current.java8Compatible ? [GradleVersion.current().version] : [] as List<String>))
+        .takeRight(2).asImmutable()
+    static final String BASELINE_PLUGIN_VERSION = '"com.monits:static-code-analysis-plugin:2.3.3"'
 
     static final int NUMBER_OF_CLASSES_TO_ANALYZE = 100
 
