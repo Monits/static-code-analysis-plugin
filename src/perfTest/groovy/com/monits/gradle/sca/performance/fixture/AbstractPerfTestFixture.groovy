f0d8b2801f6d944bdafe5d180e2be641533f92ae
Juan Martín Sotuyo Dodero <juansotuyo@gmail.com>
2019-05-27 03:51:10 -0300

    Set proper baseline for perf tests

--- src/perfTest/groovy/com/monits/gradle/sca/performance/fixture/AbstractPerfTestFixture.groovy
+++ src/perfTest/groovy/com/monits/gradle/sca/performance/fixture/AbstractPerfTestFixture.groovy
@@ -34,7 +34,7 @@ abstract class AbstractPerfTestFixture extends Specification {
     static final List<String> TESTED_GRADLE_VERSIONS_FOR_ANDROID = (['2.14.1', '3.5.1'] +
         (Jvm.current.java8Compatible ? ['4.10', GradleVersion.current().version] : [] as List<String>))
         .takeRight(2).asImmutable()
-    static final String BASELINE_PLUGIN_VERSION = '"com.monits:static-code-analysis-plugin:2.6.9"'
+    static final String BASELINE_PLUGIN_VERSION = '"com.monits:static-code-analysis-plugin:2.6.10"'
 
     static final int NUMBER_OF_CLASSES_TO_ANALYZE = 100
 
