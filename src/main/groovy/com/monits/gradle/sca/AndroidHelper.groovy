eed1ef732003749d5cae6f33b187e05430832029
Juan Martín Sotuyo Dodero <juansotuyo@gmail.com>
2020-04-15 18:28:40 -0300

    Update licenses

--- src/main/groovy/com/monits/gradle/sca/AndroidHelper.groovy
+++ src/main/groovy/com/monits/gradle/sca/AndroidHelper.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2017 Monits S.A.
+ * Copyright 2010-2020 Monits S.A.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
  * file except in compliance with the License. You may obtain a copy of the License at
