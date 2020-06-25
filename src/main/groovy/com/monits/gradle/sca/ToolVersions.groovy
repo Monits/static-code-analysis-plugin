d05ef23c7d22aa839d238bd34ee889ace70e8e4d
Juan Martín Sotuyo Dodero <juansotuyo@gmail.com>
2020-06-25 00:04:32 -0300

    Upgrade Checkstyle to 8.33

--- src/main/groovy/com/monits/gradle/sca/ToolVersions.groovy
+++ src/main/groovy/com/monits/gradle/sca/ToolVersions.groovy
@@ -26,7 +26,7 @@ final class ToolVersions {
     private final static String BACKWARDS_PMD_TOOL_VERSION = '5.1.3'
     private final static GradleVersion GRADLE_VERSION_PMD = GradleVersion.version('2.4')
 
-    private final static String LATEST_CHECKSTYLE_VERSION = '8.31'
+    private final static String LATEST_CHECKSTYLE_VERSION = '8.33'
     private final static String LATEST_CHECKSTYLE_VERSION_JAVA_7 = '6.19'
 
     private final static String SPOTBUGS_TOOL_VERSION = '4.0.6'
